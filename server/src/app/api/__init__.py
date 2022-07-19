4a9784d719c8654e4bfc5edaea51fe0c52ed946d
severintschui <asdf>
2022-07-19 21:49:34 +0200

    extending doc string

--- server/src/app/api/__init__.py
+++ server/src/app/api/__init__.py
@@ -6,7 +6,7 @@ from flask import Blueprint, Flask
 
 
 def register_blueprints(app: Flask) -> t.List[Blueprint]:
-    """ Iterate over all modules and register all Blueprint instances """
+    """ Iterate over all modules inside api folder and register all Blueprint instances """
     blueprints = []
     for _, module_name, _ in pkgutil.iter_modules(__path__):
         module = importlib.import_module(f'{__name__}.{module_name}')
